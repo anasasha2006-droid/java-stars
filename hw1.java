d4347da51176f05c661aace861d720e097e3bc27
anasasha2006-droid <anas.asha2006@gmail.com>
2025-11-01 17:04:35 +0200

    Add files via upload

---
+++ hw1.java
@@ -0,0 +1,19 @@
+
+package anas.siiiiiiiiiiii;
+
+import java.util.Scanner;
+
+
+public class hw1 {
+    public static void main(String[] args) {
+         Scanner x=new Scanner(System.in);
+         System.out.println("enter the number:");
+         int n=x.nextInt();
+         for (int i = 0; n < 6; i++) {
+             System.out.println("*");
+             n++;
+            
+        }
+    }
+    
+}
